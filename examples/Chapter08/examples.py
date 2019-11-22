540b0dbc0052a3de3746671c139a083ccae74ba6
John Whitington <john@coherentgraphics.co.uk>
2019-11-22 15:13:39 +0000

    more

--- examples/Chapter08/examples.py
+++ examples/Chapter08/examples.py
@@ -19,3 +19,29 @@ NameError: name 'z' is not defined
 >>> f(1, 2)
 6
 
+def safe_lookup(d, k):
+    try:
+        return d[k]
+    except KeyError as e:
+        print(f'FATAL ERROR: Bad key {k} in dict {d}')
+        raise e
+
+
+def repeated(e, length):
+    if length < 0: raise ValueError
+    l = []
+    for x in range(0, length): l.append(e)
+    return l
+
+
+def safe_lookup(d, k):
+    try:
+        result = d[k]
+    except Exception as e:
+        print('Unknown error in safe_lookup')
+        raise e
+    else:
+        print('key lookup succeeded')
+        return result
+
+
