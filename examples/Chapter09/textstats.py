83e5c6e9a345e81dfe9e96af978bdce80fac57f5
John Whitington <john@coherentgraphics.co.uk>
2019-11-28 17:37:17 +0000

    more

---
+++ examples/Chapter09/textstats.py
@@ -0,0 +1,22 @@
+#Text statistics: lines, characters, word, sentences.
+def is_full_stop(s):
+    return s == '.'
+
+def stats_from_file(f):
+    lines = 0
+    characters = 0
+    words = 0
+    sentences = 0
+    for line in f:
+        lines += 1
+        characters += len(line)
+        words += len(line.split())
+        sentences += len(list(filter(is_full_stop, list(line))))
+    return (lines, characters, words, sentences)
+
+def stats_from_filename(filename):
+    with open(filename) as f:
+        return stats_from_file(f)
+
+gregor_stats = stats_from_filename('gregor.txt')
+
