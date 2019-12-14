1862cd152eaa91a9ca7ee72a0b7a780612002860
John Whitington <john@coherentgraphics.co.uk>
2019-12-14 13:34:21 +0000

    more

---
+++ exercises/Chapter10/exercises.py
@@ -0,0 +1,39 @@
+import math
+
+#1
+def round(x):
+    c = math.ceil(x)
+    f = math.floor(x)
+    if c - x <= x - f:
+        return c
+    else:
+        return f
+
+#2
+def between(a, b):
+    x0, y0 = a
+    x1, y1 = b
+    return ((x0 + x1) / 2, (y0 + y1) / 2)
+
+#3
+def parts(x):
+    if x < 0:
+        a, b = parts(-x)
+        return (-a, b)
+    else
+        return (math.floor x, x - math.floor x)
+
+#4
+def star(x):
+    i = math.floor(x * 50)
+    if i == 50: i = 49
+    print(' ' * (i - 1) + '*')
+
+
+#5
+def plot(f, a, b, dy):
+  pos = a
+  while pos <= b:
+      star(f(pos))
+      pos = pos + dy
+
---
+++ exercises/Chapter11/exercises.py
@@ -0,0 +1,47 @@
+#2
+import string
+
+def string_is_integer(s):
+    is_integer = True
+    for x in s:
+      if x not in string.digits: is_integer = False
+    return is_integer
+
+#3
+import random
+import getpass
+
+def guessing_game():
+    target = int(getpass.getpass('What is the target number?'))
+    guess = int(input('Guess a number between 1 and 100\n'))
+    tries = 1
+    while guess != target:
+        tries = tries + 1
+        if guess < target:
+            guess = int(input('Higher!\n'))
+        elif guess > target:
+            guess = int(input('Lower!\n'))
+    print('Correct! You took ' + str(tries) + ' guesses.')
+
+#4
+import statistics
+
+def stats(l):
+    return (statistics.median(l), statistics.mode(l), statistics.mean(l))
+
+#5
+import time
+
+def reaction_test():
+    print('Ready? Press Enter when you see ENTER')
+    print('3')
+    time.sleep(1)
+    print('2')
+    time.sleep(1)
+    print('1')
+    time.sleep(1)
+    print('ENTER')
+    t = time.time()
+    enter = input()
+    t2 = time.time()
+    print(f'Your reaction time was {t2 - t} seconds')
