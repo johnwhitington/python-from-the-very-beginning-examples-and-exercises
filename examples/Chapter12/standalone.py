4991056ff2d097cfd3b249e58e8f8a6e0183109d
John Whitington <john@coherentgraphics.co.uk>
2020-01-01 18:30:50 +0000

    more

--- examples/Chapter12/standalone.py
+++ examples/Chapter12/standalone.py
@@ -1,6 +1,6 @@
 import sys
 
-print(f'This progam is called {sys.argv[0]}')
+print(f'This program is called {sys.argv[0]}')
 print(f'There are {len(sys.argv) - 1} command line arguments')
 for n, arg in enumerate(sys.argv[1:]):
   print(f'Argument {n} is {arg}')
