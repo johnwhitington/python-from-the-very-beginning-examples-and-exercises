3aca54c48c8a7ac6cbab29ba70f3f62723896a13
John Whitington <john@coherentgraphics.co.uk>
2020-01-01 15:10:20 +0000

    more

---
+++ examples/Chapter12/textstats.py
@@ -0,0 +1,6 @@
+import sys
+import textstat
+if len(sys.argv) > 1:
+    ls, cs, ws, ss = textstat.stats_from_filename(sys.argv[1])
+    print(f'{ls} lines, {cs} characters, {ws} words, {ss} sentences')
+    
