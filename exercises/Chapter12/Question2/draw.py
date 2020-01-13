40327b992c1e4d165700643c127b98645b9ca638
John Whitington <john@coherentgraphics.co.uk>
2020-01-13 15:32:52 +0800

    q2

---
+++ exercises/Chapter12/Question2/draw.py
@@ -0,0 +1,13 @@
+import math
+
+def star(x):
+    i = math.floor(x * 50)
+    if i == 50: i = 49
+    print(' ' * (i - 1) + '*')
+
+def plot(f, a, b, dy):
+  pos = a
+  while pos <= b:
+      star(f(pos))
+      pos = pos + dy
+
