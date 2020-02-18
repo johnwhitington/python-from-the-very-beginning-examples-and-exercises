791a316a5027995bd8b255e41c9c2b76909ebf6a
John Whitington <john@coherentgraphics.co.uk>
2020-02-18 17:40:30 +0100

    more

--- projects/Project01/answers.py
+++ projects/Project01/answers.py
@@ -28,16 +28,58 @@ def circle(r):
     poly(int(circumference), 1.0)
 
 #Question 4
-def grid(sx, sy, nx, ny)
-
+#Grid of circles starting with top-left at (sx, sy), size 50, nx wide, ny tall. 
+def grid(sx, sy, nx, ny):
+    for x in range(nx):
+        for y in range(ny):
+            t.penup()
+            t.goto(sx + x * 50, sy + y * 50)
+            t.pendown()
+            t.circle(25)
 
 #Question 5
-gamut of colours
+#We need eleven big squares, one for each 0, 0.1, ... 1.0 in red
+#Then each big square has side 11, for green vs blue.
+def filled_square(x, y, l):
+    t.penup()
+    t.goto(x, y)
+    t.begin_fill()
+    t.setheading(90)
+    for _ in range(4):
+        t.fd(l)
+        t.rt(90)
+    t.end_fill()
+
+def red_gamut(x, y, r):
+    for b in range(11):
+        for g in range(11):
+            t.color(r, g * 0.1, b * 0.1)
+            filled_square(x + b * 5, y + g * 5, 5)
+
+def whole_gamut():
+    for r in range(11):
+        red_gamut(-300 + 55 * r, 0, r * 0.1)
 
 #Question 6
-random walk
+import random
+
+def walk():
+    px = 0
+    py = 0
+    while px > -300 and px < 300 and py > -300 and py < 300:
+        dx = random.randint(-1, 1)
+        dy = random.randint(-1, 1)
+        px = px + dx
+        py = py + dy
+        t.goto(px, py)
 
 #Question 7
-#filled circle x2
+import math
 
+def filled_circle(r):
+    circumference = 2.0 * math.pi * r
+    t.penup()
+    t.begin_fill()
+    poly(int(circumference), 1.0)
+    t.end_fill()
 
---
+++ projects/Project01/project1a.py
@@ -0,0 +1,71 @@
+#graph a given function given on the command line
+#multiple ones go in different colours
+#allow setting of scales from command line
+import sys
+import turtle
+import math
+
+args = sys.argv
+
+t = turtle.Turtle()
+
+if len(sys.argv) < 2:
+    args.append(turtle.Screen().textinput('Graph plotter', 'Please enter formula'))
+
+def plot(f):
+    t.penup()
+    t.goto(-300, f(-300))
+    t.pendown()
+    for x in range(-300, 300, 1):
+        t.goto(x, f(x))
+
+def farg(arg):
+    def f(x):
+      return(eval(arg))
+    return f
+
+def key(n, formula_text, color):
+    t.color(color)
+    t.penup()
+    t.goto(-300, -200 - 20 * n)
+    t.pendown()
+    t.write(formula_text, font = ("Arial", 16, "normal"))
+
+def line(x0, y0, x1, y1):
+    t.penup()
+    t.goto(x0, y0)
+    t.pendown()
+    t.goto(x1, y1)
+
+def axes():
+    t.color("black")
+    line(-300, 0, 300, 0)
+    line(0, -300, 0, 300)
+    for x in range(-300, 301, 50):
+        if x != 0:
+            t.penup()
+            t.goto(x, -20)
+            t.pendown()
+            t.write(str(x), font = ("Arial", 12, "normal"))
+            line(x, -5, x, 5)
+    for y in range(-300, 301, 50):
+        if y != 0:
+            t.penup()
+            t.goto(-20, y)
+            t.pendown()
+            t.write(str(y), font = ("Arial", 12, "normal"))
+            line(-5, y, 5, y)
+
+colors = ["black", "red", "green", "blue"]
+
+t.speed(0)
+
+axes()
+
+for n, arg in enumerate(args[1:]):
+    t.pencolor(colors[n])
+    plot(farg(arg))
+    key(n, arg, colors[n])
+
+turtle.mainloop()
+
