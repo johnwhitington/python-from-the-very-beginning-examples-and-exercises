ee99b29a714c4ebe2c34ffcbe1f73d99bb1e581e
John Whitington <john@coherentgraphics.co.uk>
2020-02-19 11:28:14 +0100

    more

---
+++ projects/Project01/clock.py
@@ -0,0 +1,50 @@
+#Live clock
+import turtle
+import math
+import time
+
+def hand(length, thickness, angle):
+    t.penup()
+    t.home()
+    t.setheading(90)
+    t.pensize(thickness)
+    t.pendown()
+    t.rt(angle)
+    t.fd(length)
+
+def tickmarks():
+    t.pensize(1)
+    for a in range (0, 60):
+        t.penup()
+        t.home()
+        t.setheading(90)
+        t.rt(360 / 60 * a)
+        t.fd(295)
+        t.pendown()
+        t.fd(5)
+
+def clockface(h, m, s):
+    #Circle
+    t.penup()
+    t.goto(0, -300)
+    t.pendown()
+    t.circle(300)
+    #Tickmarks
+    tickmarks()
+    #Hands
+    hand(200, 3, 360 / 12 * (h % 12))
+    hand(280, 3, 360 / 60 * m)
+    hand(295, 1, 360 / 60 * s)
+
+t = turtle.Turtle()
+t.hideturtle()
+turtle.Screen().tracer(0, 0)
+
+while True:
+    tm = time.localtime()
+    t.home()
+    t.clear()
+    clockface(tm.tm_hour, tm.tm_min, tm.tm_sec)
+    turtle.Screen().update()
+    time.sleep(1)
+
