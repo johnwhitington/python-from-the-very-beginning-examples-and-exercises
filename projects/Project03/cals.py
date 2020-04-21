72341d11c7f9cf526834f50b3ecef0f95d8026c7
John Whitington <john@coherentgraphics.co.uk>
2020-04-21 15:52:37 +0100

    more

--- projects/Project03/cals.py
+++ projects/Project03/cals.py
@@ -22,8 +22,8 @@ def table_of_file(filename):
 # cals list eaten <name> <date>
 # cals list weights <name>
 def list_eaten(name, date):
-    for k, vs = table_of_file(os.path.join(name, date)):
-        print(f'{k} {vs}')
+    for k, vs in table_of_file(os.path.join(name, date) + '.txt').items():
+        print(f'{k} {vs[0]}')
 
 def list_weights(name):
     for k, vs in table_of_file(os.path.join(name, 'weight.txt')).items():
@@ -31,7 +31,9 @@ def list_weights(name):
     
 # List the dates for which we have calorie counts
 # cals list dates
-def list_dates(name): pass
+def list_dates(name):
+    for filename in sorted(os.listdir(name)):
+        if filename != 'weight.txt': print(filename[:-4])
 
 # List the calorie data itself.
 # cals list foods
@@ -53,7 +55,7 @@ def lookup_calories(food):
         if len(vs) > 1:
             weight = vs[0]
             calories = vs[1]
-            print(f'There are {calories} calories in {weight} of {food}')
+            print(f'There are {calories} calories in {weight}g of {food}')
         else:
             print(f'Malformed calorie entry for {food} in calories file')
 
@@ -63,16 +65,22 @@ def lookup_weight(name, date):
     if vs == None:
         print(f'No weight found for {date}')
     elif len(vs) > 0:
-        print(f'Weight at {date} is {vs[0]}')
+        print(f'Weight at {date} was {vs[0]}')
 
 # Print the total calories for just a given day.
 # cals total <date>
-def total_date(name, date)
-    table = table_of_file(os.path.join(name, date)
+def total_date(name, date):
+    calories = table_of_file('calories.txt')
+    table = table_of_file(os.path.join(name, date) + '.txt')
     total = 0
     for k, vs in table.items():
-        total = total + vs[0] #FIXME: multiply weight? Or already done?
-    return total
+        print(f'food is {k}, grams is {vs[0]}')
+        weight_and_calories = calories[k]
+        reference_weight = int(weight_and_calories[0])
+        reference_calories = int(weight_and_calories[1])
+        calories_per_gram = reference_calories / reference_weight
+        total += int(vs[0]) * calories_per_gram
+    print(f'Total calories for {date}: {int(total)}')
 
 # Create and initialise new user
 # cals newuser <name>
@@ -82,13 +90,13 @@ def new_user(name):
         pass
 
 def date_today():
-   date = datetime.datetime.now()
-   return (f'{d.day}-{d.month}-{d.year}')
+   d = datetime.datetime.now()
+   return (f'{d.day:02}-{d.month:02}-{d.year}')
 
 # Add data for today - food and grams
 # cals eaten <name> <food> <grams>
 def eaten(name, food, grams):
-    with open(os.path.join(name, date_today()), 'a') as f:
+    with open(os.path.join(name, date_today()) + '.txt', 'a') as f:
         print(f'{food} {grams}', file=f)
 
 # Main program. Read args and dispatch.
@@ -112,10 +120,10 @@ if len(sys.argv) > 1:
                 lookup_weight(sys.argv[3], sys.argv[4])
     elif cmd == 'total':
         if len(sys.argv) > 3:
-            total_date(sys.argv[3], sys.argv[4])
+            total_date(sys.argv[2], sys.argv[3])
     elif cmd == 'newuser':
         if len(sys.argv) > 2:
-            new_user(sys.argv[3])
+            new_user(sys.argv[2])
     elif cmd == 'eaten':
         if len(sys.argv) > 4:
-            eaten(sys.argv[3], sys.argv[4], sys.argv[5])
+            eaten(sys.argv[2], sys.argv[3], sys.argv[4])
