28df771d08ba34099376c8b23c687d43ce6eb5fc
John Whitington <john@coherentgraphics.co.uk>
2020-05-07 14:06:46 +0100

    more

---
+++ projects/Project04/image.py
@@ -0,0 +1,18 @@
+from PIL import Image
+i = Image.open('image.png')
+
+#modify an image in place, and show and re-save
+#greyscale, brightness, contrast, flip, instagram type effect
+
+#New image from old
+#rotate, blur, resize
+
+
+#make an image from scratch
+#gradient, fractals, similar?
+#from our turtle graphics?
+
+#Subtitling: adding text on top with PostScript
+
+#An animated GIF
+
