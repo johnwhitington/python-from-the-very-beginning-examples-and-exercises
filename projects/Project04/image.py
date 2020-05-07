d6393da043c8bf8fb970e932f6c4156dcbd188ee
John Whitington <john@coherentgraphics.co.uk>
2020-05-07 14:57:03 +0100

    more

--- projects/Project04/image.py
+++ projects/Project04/image.py
@@ -1,11 +1,27 @@
 from PIL import Image
 i = Image.open('image.png')
+p = i.load()
 
 #modify an image in place, and show and re-save
 #greyscale, brightness, contrast, flip, instagram type effect
 
+
 #New image from old
-#rotate, blur, resize
+#rotate, blur, resize, crop
+def grey(p):
+    r, g, b = p
+    gr = int((r + g + b) / 3)
+    return (gr, gr, gr)
+
+i2 = i.copy()
+p2 = i2.load()
+
+sx, sy = i2.size
+
+for x in range(sx):
+    for y in range(sy):
+        p2[x, y] = grey(p[x, y])
+
 
 
 #make an image from scratch
@@ -14,5 +30,5 @@ i = Image.open('image.png')
 
 #Subtitling: adding text on top with PostScript
 
-#An animated GIF
+#An animated GIF. Reverse existing ones.
 
