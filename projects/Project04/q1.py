26f67cf11750b4e35e1a03b0f6257de0b70d415e
John Whitington <john@coherentgraphics.co.uk>
2020-05-29 15:45:25 +0100

    more

--- projects/Project04/q1.py
+++ projects/Project04/q1.py
@@ -3,18 +3,24 @@ i = Image.open('rabbit.png')
 
 #Question 1
 #Write functions to increase or decrease the brightness and contrast of an image. Brightness may be achieved by simple addition of each component of each pixel by an appropriate factor, and contrast by multiplication.
+
+def clamp(x):
+    if x < 0: return 0
+    elif x > 255: return 255
+    else: return x
+
 def brightness(p, x):
   r, g, b = p
-  r_out = r
-  g_out = g
-  b_out = b
+  r_out = clamp (int (r + x * 128))
+  g_out = clamp (int (g + x * 128))
+  b_out = clamp (int (b + x * 128))
   return (r_out, g_out, b_out)
 
 def contrast(p, x):
   r, g, b = p
-  r_out = r
-  g_out = g
-  b_out = b
+  r_out = clamp (int (r * x))
+  g_out = clamp (int (g * x))
+  b_out = clamp (int (b * x))
   return (r_out, g_out, b_out)
 
 def process_pixels(f, i):
@@ -35,13 +41,13 @@ def contrast_image(i, x):
     def c(p): return contrast(p, x)
     return process_pixels(c, i)
 
-bright = brightness_image(i, 1.5)
-dim = brightness_image(i, 0.5)
-low_contrast = contrast_image(i, 0.5)
+bright = brightness_image(i, 0.5)
+dim = brightness_image(i, -0.5)
+low_contrast = contrast_image(i, 0.25)
 high_contrast = contrast_image(i, 1.5)
 
 bright.save('bright.png')
 dim.save('dim.png')
-low_contrast.save('low_contrast.png')
-high_contrast.save('high_contrast.png')
+low_contrast.save('lowcontrast.png')
+high_contrast.save('highcontrast.png')
 
