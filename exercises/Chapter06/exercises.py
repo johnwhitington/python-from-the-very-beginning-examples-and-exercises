6c8e3e0b8de6832a0e2a544e4091062839be0111
John Whitington <john@coherentgraphics.co.uk>
2020-07-21 15:35:32 +0100

    more

--- exercises/Chapter06/exercises.py
+++ exercises/Chapter06/exercises.py
@@ -31,14 +31,14 @@ def print_powers():
         print(x1, x2, x3, x4, x5)
 
 #4
-def names_to_file():
-    with open('names.txt', 'w') as f:
-        name = 'not empty'
-        while name != '':
-            name = input('Title, forename and surname, please: ')
-            if name != '':
-                words = name.split()
-                print(words[2], words[1], words[0], sep=', ', file=f)
+def print_powers():
+    for x in range(1, 10):
+        x1 = str(x).zfill(5)
+        x2 = str(x ** 2).zfill(5)
+        x3 = str(x ** 3).zfill(5)
+        x4 = str(x ** 4).zfill(5)
+        x5 = str(x ** 5).zfill(5)
+        print(x1, x2, x3, x4, x5)
 
 #5
 def names_to_file():
@@ -48,5 +48,40 @@ def names_to_file():
             name = input('Title, forename and surname, please: ')
             if name != '':
                 words = name.split()
+                print(words[2], words[1], words[0], sep=', ', file=f)
+
+#6
+def names_to_file():
+    with open('names.txt', 'w') as f:
+        name = 'not empty'
+        while name != '':
+            name = input('Title, forename and surname, please: ')
+            if name != '':
+                words = name.split()
                 print(f'{words[2]}, {words[1]}, {words[0]}', sep=', ', file=f)
+
+#7
+def number_found(sentences, word):
+    n = 0
+    for s in sentences:
+        n = n + 1
+        p = s.find(word)
+        if p == -1:
+            print(f'{word} not found in sentence {n}')
+        else:
+            print(f'{word} found at position {p} in sentence {n}')
+
+
+#8
+def number_found(sentences, word, filename):
+    n = 0
+    with open(filename, 'w') as f:
+        for s in sentences:
+            n = n + 1
+            p = s.find(word)
+            if p == -1:
+                print(f'{word} not found in sentence {n}', file=f)
+            else:
+                print(f'{word} found at position {p} in sentence {n}', file=f)
+
 
