229bff04f69229671c7e63a0be288d53cf5e9e22
John Whitington <john@coherentgraphics.co.uk>
2020-08-10 17:02:25 +0100

    more

--- examples/Chapter06/examples.py
+++ examples/Chapter06/examples.py
@@ -1,12 +1,10 @@
 def print_stats(l):
     print(str(min(l)) + ' up to ' + str(max(l)))
 
-
 def print_stats(l):
     minimum = min(l)
     maximum = max(l)
     print(f'{minimum} up to {maximum}')
-
 
 def print_stats(l):
     print(f'{min(l)} up to {max(l)}')
@@ -19,17 +17,17 @@ def print_powers():
 
 def print_powers():
     for x in range(1, 10):
-        print(f'{x} {x ** 2:5d} {x ** 3:5d} {x ** 4:5d} {x ** 5:5d}')
+        print(f'{x:5d} {x ** 2:5d} {x ** 3:5d} {x ** 4:5d} {x ** 5:5d}')
 
 
 def print_powers():
     f = open('powers.txt', 'w')
     for x in range(1, 10):
-        print(f'{x} {x ** 2:5d} {x ** 3:5d} {x ** 4:5d} {x ** 5:5d}', file=f)
+        print(f'{x:5d} {x ** 2:5d} {x ** 3:5d} {x ** 4:5d} {x ** 5:5d}', file=f)
     f.close()
 
 def print_powers():
     with open('powers.txt', 'w') as f:
         for x in range(1, 10):
-            print(f'{x} {x ** 2:5d} {x ** 3:5d} {x ** 4:5d} {x ** 5:5d}',
+            print(f'{x:5d} {x ** 2:5d} {x ** 3:5d} {x ** 4:5d} {x ** 5:5d}',
                   file=f)
