9fde62a270b1141d113cc5bef8ddce63c81555a4
John Whitington <john@coherentgraphics.co.uk>
2020-09-10 17:33:01 +0100

    more

--- examples/Chapter06/examples.py
+++ examples/Chapter06/examples.py
@@ -10,24 +10,24 @@ def print_stats(l):
     print(f'{min(l)} up to {max(l)}')
 
 
-def print_powers():
-    for x in range(1, 10):
+def print_powers(n):
+    for x in range(1, n):
         print(f'{x} {x ** 2} {x ** 3} {x ** 4} {x ** 5}')
 
 
-def print_powers():
-    for x in range(1, 10):
+def print_powers(n):
+    for x in range(1, n):
         print(f'{x:5d} {x ** 2:5d} {x ** 3:5d} {x ** 4:5d} {x ** 5:5d}')
 
 
-def print_powers():
+def print_powers(n):
     f = open('powers.txt', 'w')
-    for x in range(1, 10):
+    for x in range(1, n):
         print(f'{x:5d} {x ** 2:5d} {x ** 3:5d} {x ** 4:5d} {x ** 5:5d}', file=f)
     f.close()
 
-def print_powers():
+def print_powers(n):
     with open('powers.txt', 'w') as f:
-        for x in range(1, 10):
+        for x in range(1, n):
             print(f'{x:5d} {x ** 2:5d} {x ** 3:5d} {x ** 4:5d} {x ** 5:5d}',
                   file=f)
